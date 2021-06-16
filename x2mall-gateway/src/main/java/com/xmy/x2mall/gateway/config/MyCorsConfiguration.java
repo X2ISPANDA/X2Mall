c0d898898028932025bbb7911a5fe707287e8ef3
xmy <xmy329@gmail.com>
2021-06-16 23:00:43 +0800

    完成路由配置

---
+++ x2mall-gateway/src/main/java/com/xmy/x2mall/gateway/config/MyCorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.xmy.x2mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * Created By Yu With Intellij IDEA
+ *
+ * @Date: 2021年06月15日15:17
+ * @Description：
+ */
+@Configuration
+public class MyCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //1.配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
